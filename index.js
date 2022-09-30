b723fa21051a99b3104d209f2ffe0a153648919e
Warren Santiago <wsantiago9623@gmail.com>
2022-09-29 23:32:35 -0400

    commit tree 271f4885920878f908dcae25dd047953ba7e70ef

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+//Array1 = ["Antonia", "Nuru", "Amari", "Nuru"]
+
+const returnFirstTwoDrivers = function()
+{ ["Antonia", "Nuru", "Amari", "Mo"];
+    return [ "Antonia", "Nuru" ];
+    
+    
+} 
+    
+
+    
+
+    
+
+
+const returnLastTwoDrivers = function() 
+{["Antonia", "Nuru", "Amari", "Mo"];
+    return [ "Amari", "Mo" ];
+
+}
+
+const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
+
+
+
+//createFareMultiplier()
+
+//fareDoubler()
+
+//fareTripler()
+
+//selectDifferentDrivers(arrayOfDrivers, function)
